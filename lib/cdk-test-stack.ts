875f2fb610e8deefd09279259e292c19e13b0e9c
nomi3 <thisisnomi3@gmail.com>
2020-02-15 13:49:18 +0900

    first commit

--- lib/cdk-test-stack.ts
+++ lib/cdk-test-stack.ts
@@ -1,9 +1,62 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from '@aws-cdk/core'
+// 使用するライブラリをインポート
+import * as lambda from '@aws-cdk/aws-lambda'
+import * as apigateway from '@aws-cdk/aws-apigateway'
+import * as dynamodb from '@aws-cdk/aws-dynamodb'
+import * as iam from '@aws-cdk/aws-iam'
 
 export class CdkTestStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    cdk.Tag.add(this, 'use-case', 'workshop')
 
-    // The code that defines your stack goes here
+    const tableName = 'cdk-test-table';
+
+    // Lambdaの定義
+    const cdkTestFunction = new lambda.Function(this, 'cdk-test-function', {
+      // 必須の項目
+      runtime: lambda.Runtime.NODEJS_12_X,
+      handler: 'index.handler',
+      code: lambda.Code.asset('lambda'),
+      // オプション項目
+      functionName: 'cdk-test-function',
+      environment:{
+        "TABLE_NAME": tableName
+      }
+    })
+
+    // API Gatewayの定義
+    const cdkTestApi = new apigateway.RestApi(this, 'cdk-test-api', {
+      // オプション項目
+      restApiName: 'cdk-test-api',
+      deployOptions: {
+        stageName: 'test'
+      }
+    })
+
+    // API gateway→Lambdaの接続定義
+    const integration = new apigateway.LambdaIntegration(cdkTestFunction, {
+      allowTestInvoke: false
+    })
+    cdkTestApi.root.addMethod('POST', integration)
+    cdkTestApi.root.addMethod('GET', integration)
+
+    // DynamoDBの定義
+    const cdkTestTable = new dynamodb.Table(this, 'cdk-test-table', {
+      // 必須の項目
+      partitionKey: { name: 'user_id', type: dynamodb.AttributeType.NUMBER },
+      // オプション項目
+      tableName: tableName
+    })
+
+    //Lambda→DynamoDBのアクセスポリシーの定義
+    cdkTestFunction.addToRolePolicy(new iam.PolicyStatement({
+      resources: [cdkTestTable.tableArn],
+      actions: [
+        'dynamodb:GetItem',
+        'dynamodb:PutItem'
+      ]
+    }
+    ))
   }
 }
